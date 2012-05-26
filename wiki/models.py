d931d6420e784411e72233d8e7bb9a49358a4a98
Riccardo Carlesso <riccardo_carlesso@dell.com>
2012-05-26 08:57:53 +0200

    Added Wiki model to DB.

--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Page(models.Model):
-    name   = models.CharField(maxlength="20", primary_key=True) # so this IS the primary key
+    name    = models.CharField(max_length="20", primary_key=True) # so this IS the primary key
     content = models.TextField(blank=True)
--- wikicamp/settings.py
+++ wikicamp/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     #'wikicamp.wiki',
+    'wiki',
 )
 
 # A sample logging configuration. The only tangible logging
